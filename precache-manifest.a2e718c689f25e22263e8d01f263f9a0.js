f594dc782c7ba97ce2c0b9b5a0e200d155c8b1e8
Joshua Ramos <rjosh4457@gmail.com>
2018-10-27 19:30:30 +0800

    Updates

---
+++ precache-manifest.a2e718c689f25e22263e8d01f263f9a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fb6455d9ec1a52a6a80",
+    "url": "/my-portfolio/static/css/main.6b8dc547.chunk.css"
+  },
+  {
+    "revision": "8fb6455d9ec1a52a6a80",
+    "url": "/my-portfolio/static/js/main.8fb6455d.chunk.js"
+  },
+  {
+    "revision": "21399215d3ba7c5cf633",
+    "url": "/my-portfolio/static/js/1.21399215.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "0dc70f1658a38c3a771cc0b43df0965f",
+    "url": "/my-portfolio/static/media/pier-background.0dc70f16.jpg"
+  },
+  {
+    "revision": "fedb14a87c23a3b65460ee8538887394",
+    "url": "/my-portfolio/index.html"
+  }
+];
